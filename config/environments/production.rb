e4d67a38f177e0fc4562a50c35f965c006b4ca94
Nakamura shinichirou <naka5313@gmail.com>
2012-08-17 16:51:13 +0900

    update mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ TaksBoard::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.ation_mailer.default_url_options = {:host=>'smtp.sendgrid.net'}
 
   # Enable threaded mode
   # config.threadsafe!
