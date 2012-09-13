229c4de8a9ab33f471249c61df77171cdb3d7971
Nakamura shinichirou <naka5313@gmail.com>
2012-09-14 00:00:09 +0900

    top update

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,8 @@
 class TopController < ApplicationController
   layout "users"
 
+  caches_page :index
+
   def index
     @tasks = Task.where("status=?",1).where('public_flag=?',1).where('r_user_id=?',0).latest.paginate(:per_page =>20, :page => params[:page])
   end
--- app/models/task_sweeper.rb
+++ app/models/task_sweeper.rb
@@ -4,12 +4,12 @@ class TaskSweeper < ActionController::Caching::Sweeper
   observe Task
   
   def after_save(task)
-    expire_page :action=>:index
+    expire_page :action=>:index, :controller=>'top'
     expire_page :action=>:show,:id=>task.id
   end
   
   def after_destroy(task)
-    expire_page :action=>:index
+    expire_page :action=>:index, :controller=>'top'
     expire_page :action=>:show,:id=>task.id
   end
 end
