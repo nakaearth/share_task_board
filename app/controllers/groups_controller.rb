0101e177a024cdfa335d8a984504169c02efa2ea
nakamura shinichirou <naka5313@gmail.com>
2012-09-18 00:20:06 +0900

    update controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,13 @@
 class GroupsController < ApplicationController
   def index
+    @groups=Group.paginate(:page=>params[:page], :per_page=>20)
   end
 
   def show
   end
 
   def new
+    @group=Group.new
   end
 
   def create
