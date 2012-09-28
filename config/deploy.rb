17d8dba21d223a9379b6711a42444afdfc9d0418
nakamura shinichirou <naka5313@gmail.com>
2012-09-28 15:25:27 +0900

    update config/deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,8 @@ set :keep_releases,3
 
 role :web, "127.0.0.1:22"                          # Your HTTP server, Apache/etc
 role :app, "127.0.0.1:22"                          # This may be the same as your `Web` server
-#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
+role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -19,23 +19,17 @@ role :app, "127.0.0.1:22"                          # This may be the same as you
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-namespace :local do
-  task :deploy do
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start,:roles=> :app do
+    run "./script/start_local_server.sh"
     run "cp /usr/local/project/release"
+    send(run_method,"./script/start_local_server.sh")
+  end
+  task :stop,:roles=>:app do
+    run "./script/stop_local_server.sh"
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
-
-# If you are using Passenger mod_rails uncomment this:
-#namespace :deploy do
-#  task :start,:roles=> :app do
-#    run "./script/start_local_server.sh"
-#    run "cp /usr/local/project/release"
-#    send(run_method,"./script/start_local_server.sh")
-#  end
-#  task :stop,:roles=>:app do
-#    run "./script/stop_local_server.sh"
-#  end
-#  task :restart, :roles => :app, :except => { :no_release => true } do
-#    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#  end
-#end
