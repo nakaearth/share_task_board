ab4ba8dab2b900499fdca078d2b59cd4cc793289
nakamura shinichirou <naka5313@gmail.com>
2012-10-05 17:55:05 +0900

    update layout files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
+
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
 
   def update 
     Cloudinary::Uploader.upload(params[:avatar])
-
     @user=User.find(current_user.id)
     @user.avatar=params[:avatar]
     @user.save
