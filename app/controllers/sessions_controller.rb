8afda1153f86a18aadb7eb984e563fe70840d47f
nakamura shinichirou <naka5313@gmail.com>
2012-10-23 19:13:20 +0900

    update files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     auth=request.env["omniauth.auth"]
     @user=User.where(provider: auth["provider"],uid: auth["uid"]).first || User.create_with_omniauth(auth)
     session[:user_id]=@user.id
-    redirect_to action: 'index', controller: 'jobs', notice: "login success."
+    redirect_to action: 'index', controller: 'top', notice: "login success."
   end
 
   def destroy
