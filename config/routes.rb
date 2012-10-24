73f79657f04e15c1837217e4af25dbbd82effbd6
nakamura shinichirou <naka5313@gmail.com>
2012-10-24 20:01:09 +0900

    update files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 TaksBoard::Application.routes.draw do
   get "top/index"
-  match "/login" => "sessions#new",:as => :login
+  get "/login" => "sessions#new",:as => :login
   match "/auth/:provider/callback" => "sessions#create"
   match "/auth/failure" => "sessions#failure"
   match "/logout" => "sessions#destroy"
