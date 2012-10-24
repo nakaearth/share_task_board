81a3b4ffceeaf66064b6956f452c73d4ac8291ea
nakamura shinichirou <naka5313@gmail.com>
2012-10-24 18:34:35 +0900

    file comit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     @user=User.where("provider=?", auth["provider"]).where("uid=?", auth["uid"]).first || User.create_with_omniauth(auth)
     session[:user_id]=@user.id
     p @user
-    redirect_to :root,notice: "login success."
+    redirect_to back_to || '/'
   end
 
   def destroy
@@ -15,5 +15,15 @@ class SessionsController < ApplicationController
 
   def failuer
     render text: "<span style='color: red;'>Auth Failure</span>"
+  end
+
+  def back_to
+    if request.env['omniauth.origin'].presence && back_to = CGI.unescape(request.env['omniauth.origin'].to_s)
+      uri = URI.parse(back_to)
+      return back_to if uri.relative? || uri.host == request.host
+    end
+    nil
+  rescue
+    nil
   end
 end
