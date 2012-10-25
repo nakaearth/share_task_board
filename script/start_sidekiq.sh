f1a5f89479bcbd519e36f5918b4e6399f2547861
nakamura shinichirou <naka5313@gmail.com>
2012-10-25 17:29:52 +0900

    update sidekiq setting

--- script/start_sidekiq.sh
+++ script/start_sidekiq.sh
@@ -2,6 +2,6 @@
 
 echo "===redius start===="
 redis-server /usr/local/etc/redis.conf > log/redis.log
-bundle exec sidekiq -q high,5 default > log/sidekiq.log
+bundle exec sidekiq > log/sidekiq.log
 
 
