1765193f58b00255eee7e1c5556b5712de2191fa
nakamura shinichirou <naka5313@gmail.com>
2012-10-26 23:36:59 +0900

    udpate fiies

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,11 +1,12 @@
 #coding: utf-8
 
 class Job < ActiveRecord::Base
+  include ActiveModel::ForbiddenAttributesProtection
   #elastic search
   include Tire::Model::Search
   include Tire::Model::Callbacks
 
-  attr_accessible :description, :status, :title,:priority,:public_flag
+  #attr_accessible :description, :status, :title,:priority,:public_flag
   
   scope :latest,-> {order('updated_at desc') }
   scope :todo, ->{where('status=?',1) }
