41a7d89a12f73948708cec6b611e002a2911bf12
nakamura shinichirou <naka5313@gmail.com>
2012-10-31 14:44:05 +0900

    add dyno check rb

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
-worker: bundle exec sidekiq -q high, 5 -q default
+clockwork: bundle exec clockwork ./app/workers/dyno_check.rb
+
---
+++ app/workers/dyno_check.rb
@@ -0,0 +1,19 @@
+#coding: utf-8
+
+require 'heroku'
+include 'Clockwork'
+
+clinet=Heroku::Clinet.new(*Heroku::Auth.read_credentials)
+
+handler do |job|
+  case job
+  when 'dyno.up'
+    client.ps_scale('sharetaskboard',:type => :worker, :qty=>1)
+  when 'dyno.down'
+    client.ps_scale('sharetaskboard',:type => :worker, :qty=>0)
+  end
+end
+
+every(1.days, 'dyno.up', at: '10:00')
+every(1.days, 'dyno.down', at: '18:00')
+
