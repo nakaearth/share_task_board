23987e69fb4129e79ee15ddc7597bd9f63d29105
nakamura shinichirou <naka5313@gmail.com>
2012-11-02 01:34:21 +0900

    update use method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
 
   def search_user
+    @group=Group.find(params[:group_id])
     user=User.new
     @users=user.search_user params
-    @group=Group.find(params[:group_id]) 
   end
 
   def set_user
     #user set to group
-    redirect_to action: 'show', controller: 'groups', id: params[:group_id]
+    group=Group.find(params[:group_id])
+    @user=User.find(params[:user_id])
+    @user.groups << group
+    if @user.save
+      redirect_to action: 'show', controller: 'groups', id: params[:group_id]
+    else
+      render action: 'show', controller: 'groups', id: params[:group_id]
+    end
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,14 @@ class User < ActiveRecord::Base
 
   def self.create_with_omniauth(auth)
     @user=User.new
-    @user[:provider]=auth["provider"]
-    @user[:uid]=auth["uid"]
+    @user.provider=auth["provider"]
+    @user.uid=auth["uid"]
     unless auth["info"].blank?
-      @user[:name]=auth["info"]["name"]
-      @user[:screen_name]=auth["info"]["nickname"]
-      @user[:image]=auth["info"]["image"]
+      @user.name=auth["info"]["name"]
+      @user.screen_name=auth["info"]["nickname"]
+      @user.image=auth["info"]["image"]
     end 
-    @user.save
+    @user.save!
     @user 
   end
 
@@ -28,8 +28,12 @@ class User < ActiveRecord::Base
   end
 
   def search_user(params)
+    user_id_array=GroupMap.where('group_id=?',params[:group_id]).pluck(:user_id)
+    user_list=user_id_array.join(',')
     @users=User.where('provider =?',params[:provider])
                .where('name like ?',"#{params[:name]}%")
-               .paginate(:page=>params[:page], :per_page=>params[:per_page])  
+               .where(['id not in(?)',user_id_array])
+               .paginate(:page=>params[:page], :per_page=>params[:per_page]) 
+     
   end
 end
