982489e01702253e230f433272032953c458ff19
nakamura shinichirou <naka5313@gmail.com>
2012-12-07 23:40:30 +0900

    spec file update.job_controlller error case add.

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -25,6 +25,18 @@ describe JobsController do
     }
   end
 
+  def error_attribute
+    {
+      :title=>'test',
+      :description=>'test test test',
+      :status=>'1',
+      :priority=>'1',
+      :group_id=>'1',
+      :public_flag=>'0',
+      :r_user_id=>'0'
+    }
+  end
+
   describe "GET index" do
     fixtures :users
     fixtures :jobs
@@ -82,7 +94,7 @@ describe JobsController do
     end
   end
 
-  describe "POST create" do
+  describe "new job save" do
     fixtures :users
     fixtures :jobs
     fixtures :groups
@@ -93,7 +105,7 @@ describe JobsController do
         controller.stub(:current_user) {@user}
       end
       it "assigns a newly created task as @job" do
-        post :create,{:job=> valid_attribute},valid_session
+        post :create,{:job=> valid_attribute}
         assigns(:job).should be_a(Job)
         assigns(:job).should be_persisted
       end
@@ -103,80 +115,41 @@ describe JobsController do
       end
     end
   end
-#    describe "with invalid params" do
-#      it "assigns a newly created but unsaved job as @job" do
-#        # Trigger the behavior that occurs when invalid params are submitted
-#        Job.any_instance.stub(:save).and_return(false)
-#        post :create, {:job => {}}, valid_session
-#        assigns(:job).should be_a_new(Job)
-#      end
-#
-#      it "re-renders the 'new' template" do
-#        # Trigger the behavior that occurs when invalid params are submitted
-#        Job.any_instance.stub(:save).and_return(false)
-#        post :create, {:job => {}}, valid_session
-#        response.should render_template("new")
-#      end
-#    end
-#  end
+  describe "new job create error" do
+    fixtures :users
+    fixtures :jobs
+    fixtures :groups
 
-##########################################
-#  describe "PUT update" do
-#    describe "with valid params" do
-#      it "updates the requested task" do
-#        task = Task.create! valid_attributes
-#        # Assuming there are no other tasks in the database, this
-#        # specifies that the Task created on the previous line
-#        # receives the :update_attributes message with whatever params are
-#        # submitted in the request.
-#        Task.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-#        put :update, {:id => task.to_param, :task => {'these' => 'params'}}, valid_session
-#      end
-#
-#      it "assigns the requested task as @task" do
-#        task = Task.create! valid_attributes
-#        put :update, {:id => task.to_param, :task => valid_attributes}, valid_session
-#        assigns(:task).should eq(task)
-#      end
-#
-#      it "redirects to the task" do
-#        task = Task.create! valid_attributes
-#        put :update, {:id => task.to_param, :task => valid_attributes}, valid_session
-#        response.should redirect_to(task)
-#      end
-#    end
-#
-#    describe "with invalid params" do
-#      it "assigns the task as @task" do
-#        task = Task.create! valid_attributes
-#        # Trigger the behavior that occurs when invalid params are submitted
-#        Task.any_instance.stub(:save).and_return(false)
-#        put :update, {:id => task.to_param, :task => {}}, valid_session
-#        assigns(:task).should eq(task)
-#      end
-#
-#      it "re-renders the 'edit' template" do
-#        task = Task.create! valid_attributes
-#        # Trigger the behavior that occurs when invalid params are submitted
-#        Task.any_instance.stub(:save).and_return(false)
-#        put :update, {:id => task.to_param, :task => {}}, valid_session
-#        response.should render_template("edit")
-#      end
-#    end
-#  end
-#
-#  describe "DELETE destroy" do
-#    it "destroys the requested task" do
-#      task = Task.create! valid_attributes
-#      expect {
-#        delete :destroy, {:id => task.to_param}, valid_session
-#      }.to change(Task, :count).by(-1)
-#    end
-#
-#    it "redirects to the tasks list" do
-#      task = Task.create! valid_attributes
-#      delete :destroy, {:id => task.to_param}, valid_session
-#      response.should redirect_to(tasks_url)
-#    end
-#  end
+    describe "with valid params" do
+      before do
+        @user = User.find(1)
+        controller.stub(:current_user) {@user}
+      end
+      it "assigns a newly created task as @job" do
+        post :create,{:job=> error_attribute}
+        assigns(:job).should be_a(Job)
+        assigns(:job).should have(1).error_on(:title)
+      end
+      it "redirects to the created job" do
+        @params=valid_attribute
+        @params[:title]="testt"
+        @params[:description]=""
+
+        post :create, {:job => @params}
+        assigns(:job).should have(2).error_on(:description)
+      end
+      it "redirects to the created job" do
+        @params=valid_attribute
+        @params[:title]=""
+        @params[:description]=""
+
+        post :create, {:job => @params}
+        assigns(:job).should have(2).error_on(:title)
+        assigns(:job).should have(2).error_on(:description)
+      end
+     
+    end
+  end
+
+
 end
