ad979a93d10c731a573762ca48f4cba6030616bc
nakamura shinichirou <naka5313@gmail.com>
2012-12-22 22:32:50 +0900

    travice.yml update

--- app/workers/dyno_check.rb
+++ app/workers/dyno_check.rb
@@ -1,19 +1,19 @@
 #coding: utf-8
 
-require 'heroku'
-include Clockwork
+#require 'heroku'
+#include Clockwork
 
-clinet=Heroku::Client.new(*Heroku::Auth.read_credentials)
+#clinet=Heroku::Client.new(*Heroku::Auth.read_credentials)
 
-handler do |job|
-  case job
-  when 'dyno.up'
-    client.ps_scale('sharetaskboard',:type => :worker, :qty=>1)
-  when 'dyno.down'
-    client.ps_scale('sharetaskboard',:type => :worker, :qty=>0)
-  end
-end
+#handler do |job|
+#  case job
+#  when 'dyno.up'
+#    client.ps_scale('sharetaskboard',:type => :worker, :qty=>1)
+#  when 'dyno.down'
+#    client.ps_scale('sharetaskboard',:type => :worker, :qty=>0)
+#  end
+#end
 
-every(1.days, 'dyno.up', at: '10:00')
-every(1.days, 'dyno.down', at: '18:00')
+#every(1.days, 'dyno.up', at: '10:00')
+#every(1.days, 'dyno.down', at: '18:00')
 
