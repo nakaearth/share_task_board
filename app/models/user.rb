1c7f3164ed5c1268d3c0dc7c1c3b34ea69bcabe3
nakamura shinichirou <naka5313@gmail.com>
2013-01-23 00:28:53 +0900

    user/finder.rb add.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ActiveRecord::Base
 
   validate :avatar, :file_size =>{:maximum=>0.5.megabytes.to_i }
 
+  include User::Finder
+
   def self.create_with_omniauth(auth)
     @user=User.new
     @user.provider=auth["provider"]
@@ -53,22 +55,6 @@ class User < ActiveRecord::Base
     @groups=self.groups
   end
 
-  #JOB関連
-  def my_job_list(per_count)
-    begin
-      @todo_jobs=self.jobs.todo.latest.limit(per_count)
-      @doing_jobs=self.jobs.doing.latest.limit(per_count)
-      @done_jobs=self.jobs.done.latest.limit(per_count)
-      [@todo_jobs, @doing_jobs, @done_jobs]
-    rescue ActiveRecord::RecordNotFound 
-      logger.error("My job data not found.")
-      []
-    rescue RuntimeError => e
-      logger.error("application error!:"+e.message)
-      []
-    end
-  end
-
   def my_receive_job_list(per_count)
     begin
       @todo_jobs=Job.todo.where('r_user_id=?',self.id).latest.limit(per_count)
@@ -81,36 +67,6 @@ class User < ActiveRecord::Base
     rescue RuntimeError => e
       logger.error("application error!:"+e.message)
       []
-    end
-  end
-
-  def my_group_job_list(group_id, per_count)
-    begin
-      @todo_jobs=self.jobs.todo.where('group_id=?',group_id).latest.limit(per_count)
-      @doing_jobs=self.jobs.doing.where('group_id=?',group_id).latest.limit(per_count)
-      @done_jobs=self.jobs.done.where('group_id=?',group_id).latest.limit(per_count)
-      [@todo_jobs, @doing_jobs, @done_jobs]
-    rescue ActiveRecord::RecordNotFound 
-      logger.error("My group job data not found.")
-      []
-    rescue RuntimeError => e
-      logger.error("application error!:"+e.message)
-      []
-    end
-  end
-
-  def job_report
-    begin
-      @todo_jobs = self.jobs.todo
-      @doing_jobs = self.jobs.doing
-      @done_jobs = self.jobs.done
-      @pending_jobs = self.jobs.pending
-      @finish_jobs = self.jobs.finish
-      {todo: @todo_jobs, doing: @doing_jobs, done: @done_jobs, pending: @pending_jobs, finish: @finish_jobs}
-    rescue ActiveRecord::RecordNotFound => are 
-      logger.error "recort not fount:"  + are.message
-    rescue RuntimeError => e
-      logger.error "Application error:" + e.message
     end
   end
 
---
+++ app/models/user/finder.rb
@@ -0,0 +1,57 @@
+#coding: utf-8
+
+module User::Finder
+         extend ActiveSupport::Concern
+  included do
+    has_many :jobs
+
+    #JOB関連
+    def my_job_list(per_count)
+      begin
+        @todo_jobs  = self.jobs.todo.latest.limit(per_count)
+        @doing_jobs = self.jobs.doing.latest.limit(per_count)
+        @done_jobs  = self.jobs.done.latest.limit(per_count)
+        [@todo_jobs, @doing_jobs, @done_jobs]
+      rescue ActiveRecord::RecordNotFound
+        logger.error("My job data not found.")
+        []
+      rescue RuntimeError => e
+        logger.error("application error!:"+e.message)
+        []
+      end
+    end
+
+    #Group関連のJob一覧 
+    def my_group_job_list(group_id, per_count)
+      begin
+        @todo_jobs   = self.jobs.todo.where('group_id=?',group_id).latest.limit(per_count)
+        @doing_jobs  = self.jobs.doing.where('group_id=?',group_id).latest.limit(per_count)
+        @done_jobs   = self.jobs.done.where('group_id=?',group_id).latest.limit(per_count)
+        [@todo_jobs, @doing_jobs, @done_jobs]
+      rescue ActiveRecord::RecordNotFound 
+        logger.error("My group job data not found.")
+        []
+      rescue RuntimeError => e
+        logger.error("application error!:"+e.message)
+        []
+      end
+    end
+    
+    #Jobレポート用のメソッド
+    def job_report
+      begin
+        @todo_jobs  = self.jobs.todo
+        @doing_jobs = self.jobs.doing
+        @done_jobs  = self.jobs.done
+        @pending_jobs = self.jobs.pending
+        @finish_jobs  = self.jobs.finish
+        {todo: @todo_jobs, doing: @doing_jobs, done: @done_jobs, pending: @pending_jobs, finish: @finish_jobs}
+      rescue ActiveRecord::RecordNotFound => are
+        logger.error "recort not fount:"  + are.message
+      rescue RuntimeError => e
+        logger.error "Application error:" + e.message
+      end
+    end
+
+  end
+end 
